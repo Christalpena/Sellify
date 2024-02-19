5cf7bc2ffcf6f374a9632f0e051b11fe14846b1a
Christalpena <christalperez0@gmail.com>
2024-02-19 09:43:58 -0400

    Saving the post in the data base

--- userAccount/views.py
+++ userAccount/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import PostForm
 
 # Create your views here.
@@ -7,7 +7,11 @@ def user_account(request,username):
     form = PostForm
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
-        print("Holaa")
         if form.is_valid():
-            print("holaaa")
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect(f'/user/{request.user.username}')
+        else:
+            form = PostForm(request.POST, request.FILES)
     return render(request,'UserAccount.html',{'form':form})
